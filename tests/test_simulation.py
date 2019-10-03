80922e740f7465547bf3954e43f50b339c21bc79
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-03 16:17:10 -0700

    remove assertNone

---
+++ tests/test_simulation.py
@@ -0,0 +1,15 @@
+import unittest
+
+from virus import Virus
+from simulation import Simulation
+
+class TestSuite(unittest.TestCase):
+    def setUp(self):
+        self.virus = Virus("big V", .5, .75)
+        self.norm_simulation = Simulation(100, .1, self.virus)
+
+    def test_newly_infected(self):
+        pass
+
+    def test_create_population(self):
+        
