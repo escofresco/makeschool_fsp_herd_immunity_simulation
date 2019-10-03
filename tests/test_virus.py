e2ba8852b754aab19875dcb05610cc7a99055fea
Donald Vallejo <donny.vallejo@gmail.com>
2019-10-03 16:50:34 -0700

    finsihed test_person

--- tests/test_virus.py
+++ tests/test_virus.py
@@ -2,15 +2,17 @@ import unittest
 
 from virus import Virus
 
+
 class TestSuite(unittest.TestCase):
     def test_virus_init_method(self):
         name = "big cough"
-        repro_rate = .99
-        mortality_rate = .98
+        repro_rate = 0.99
+        mortality_rate = 0.98
         virus = Virus(name, repro_rate, mortality_rate)
         self.assertEqual(virus.name, name)
         self.assertEqual(virus.repro_rate, repro_rate)
         self.assertEqual(virus.mortality_rate, mortality_rate)
+
 
 if __name__ == "__main__":
     unittest.main()
