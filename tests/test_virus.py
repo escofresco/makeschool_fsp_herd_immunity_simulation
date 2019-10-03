7661b6bc32b5c2ceba5cf92a95c81e6d3f21f4c5
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-03 15:04:35 -0700

    simple identity test

---
+++ tests/test_virus.py
@@ -0,0 +1,16 @@
+import unittest
+
+from virus import Virus
+
+class TestSuite(unittest.TestCase):
+    def test_virus_init_method(self):
+        name = "big cough"
+        repro_rate = .99
+        mortality_rate = .98
+        virus = Virus(name, repro_rate, mortality_rate)
+        self.assertEqual(virus.name, name)
+        self.assertEqual(virus.repro_rate, repro_rate)
+        self.assertEqual(virus.mortality_rate, mortality_rate)
+
+if __name__ == "__main__":
+    unittest.main()
