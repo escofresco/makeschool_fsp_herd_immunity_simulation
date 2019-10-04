7d1bed90b9a7ee9412500a874025ebe724bb1427
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-04 14:53:31 -0700

    delete todos; finish write metadata

--- logger.py
+++ logger.py
@@ -8,9 +8,7 @@ class Logger(object):
     # test them one by one as you write your class.
 
     def __init__(self, file_name):
-        # TODO:  Finish this initialization method. The file_name passed should be the
-        # full file name of the file that the logs will be written to.
-        self.file_name = None
+        self.file_name = file_name
 
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate, basic_repro_num):
         self.pop_size = pop_size
@@ -21,17 +19,15 @@ class Logger(object):
 
         '''The simulation class should use this method immediately to log the specific
         parameters of the simulation as the first line of the file.'''
-        # TODO: Finish this method. This line of metadata should be tab-delimited
-        # it should create the text file that we will store all logs in.
-        # TIP: Use 'w' mode when you open the file. For all other methods, use
-        # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
-        # NOTE: Make sure to end every line with a '/n' character to ensure that each
-        # event logged ends up on a separate line!
 
         #write/append text to file and open it
-        with open("temp_file.txt", "a+") as temp_file:
-            temp_file.write("text appended \n")
-        pass
+        with open("temp_file.txt", "a") as temp_file:
+            temp_file.write('\t'.join(
+                [self.pop_size,
+                 self.vacc_percentage,
+                 self.virus_name,
+                 self.mortality_rate,
+                 self.basic_repro_num]))
 
     def log_interaction(self, person, random_person, random_person_sick=None, random_person_vacc=None, did_infect=None):
         self.person = person
@@ -51,9 +47,9 @@ class Logger(object):
         # exactly what happened in the interaction and create a String, and write to your logfile.
 
         if self.person.is_vaccinated:
-            print(f"{person.ID} didn't infect {random_person.ID} because {person.is_vaccinated or 'already sick'} \n")
+            print(f"{person._id} didn't infect {random_person._id} because {person.is_vaccinated or 'already sick'} \n")
         else:
-            print(f"{person.ID} infects {random_person.ID} \n")
+            print(f"{person._id} infects {random_person._id} \n")
         pass
 
     def log_infection_survival(self, person, did_die_from_infection):
