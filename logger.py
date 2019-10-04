78ce9960dfef255978c7ab790a8d5973cc5054bb
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-04 16:14:01 -0700

    move func to different class

--- logger.py
+++ logger.py
@@ -50,15 +50,6 @@ class Logger(object):
         """
         self.log_file.write(f"{person._id} {died if did_die_from_infection else survived} infection.\n")
 
-    def resolve_infection(self):
-        # if person survives infection print()
-        if self.did_die_from_infection:
-            print(f"{self.person.ID} died from infection\n"
-                  or f"{self.person.ID} survived infection. \n")
-        else:
-            print(f"{self.person.ID} survived infection. \n")
-        pass
-
     def log_time_step(self, time_step_number):
         """ STRETCH CHALLENGE DETAILS:
 
