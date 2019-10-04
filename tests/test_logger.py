931144ef754f8a53f3327a9d907bf8377112b18f
Jonasz Karwacki <jonaszakr@gmail.com>
2019-10-03 17:06:51 -0700

    add test to fill in

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,9 +1,33 @@
+import os
 import unittest
 
 from logger import Logger
 
 class TestSuite(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.temp_file_name = 'temp_logs.txt'
+        temp = open(self.temp_file_name, 'w+')
+
+    def tearDown(self):
+        os.remove('temp_logs.txt')
+
+    def test_logger_initialization(self):
+        pass
+
+    def test_writes_metadata_to_new_file(self):
+        pass
+
+    def test_writes_metadata_to_existing_file(self):
+        pass
+
+    def test_interaction_is_logged(self):
+        pass
+
+    def test_infection_survival_is_logged(self):
+        pass
+
+    def test_time_step(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
